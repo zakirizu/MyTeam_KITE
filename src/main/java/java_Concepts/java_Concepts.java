d4d0daf772aad407dcefa4cd8f937bd80c874802
Zakir <79053961+zakirizu@users.noreply.github.com>
2025-07-23 16:31:23 +0530

    The One

---
+++ src/main/java/java_Concepts/java_Concepts.java
@@ -0,0 +1,11 @@
+package java_Concepts;
+
+public class java_Concepts {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Bismillah");
+
+	}
+
+}
