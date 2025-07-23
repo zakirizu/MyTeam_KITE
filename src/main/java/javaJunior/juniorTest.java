c62e0ae341aa60d5dcd1c8c46d39c78335f5be65
Aman <amanayaan10@gmail.com>
2025-07-23 19:33:55 +0530

    Update test 2

--- src/main/java/javaJunior/juniorTest.java
+++ src/main/java/javaJunior/juniorTest.java
@@ -5,6 +5,8 @@ public class juniorTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method 
 		System.out.println("Junior - Test");
+		
+		System.out.println("Junior - Test - 02");
 
 	}
 
