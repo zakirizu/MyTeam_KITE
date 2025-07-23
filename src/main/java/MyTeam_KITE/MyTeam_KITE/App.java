59da06c99fdbce6c7ea37860f097c5884d335260
Zakir <79053961+zakirizu@users.noreply.github.com>
2025-07-23 16:28:35 +0530

    Initial commit

---
+++ src/main/java/MyTeam_KITE/MyTeam_KITE/App.java
@@ -0,0 +1,10 @@
+package MyTeam_KITE.MyTeam_KITE;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/MyTeam_KITE/MyTeam_KITE/AppTest.java
@@ -0,0 +1,19 @@
+package MyTeam_KITE.MyTeam_KITE;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
