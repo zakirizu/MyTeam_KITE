7d5b607c0b535485b8be7f6fa9d0ad2c8c2e6a59
Zakir <79053961+zakirizu@users.noreply.github.com>
2025-07-23 17:43:04 +0530

    updated

--- src/main/java/java_Concepts/java_Concepts.java
+++ src/main/java/java_Concepts/java_Concepts.java
@@ -5,6 +5,7 @@ public class java_Concepts {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Bismillah");
+		System.out.println("Legion");
 
 	}
 
